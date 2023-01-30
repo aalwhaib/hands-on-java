6ba99bbad6e91f24d3bac3c45bee6213ad30876b
Ahmed Alwahib <aalwheep@gmail.com>
2023-01-30 21:14:00 +0300

    #03_end

--- src/main/java/bank/Menu.java
+++ src/main/java/bank/Menu.java
@@ -42,6 +42,46 @@ public class Menu {
   }
 
   private void showMenu(Customer customer, Account account) {
+    int selection = 0;
 
+    while (selection != 4 && customer.isAuthenticated()) {
+      System.out.println("================================================");
+      System.out.println("Please select one of the following options: ");
+      System.out.println("1: Deposit");
+      System.out.println("2: Withdraw");
+      System.out.println("3: Check Balance");
+      System.out.println("4: Exit");
+      System.out.println("================================================");
+
+      selection = scanner.nextInt();
+      double amount = 0;
+
+      switch (selection) {
+        case 1:
+          System.out.println("How much would you like to deposit?");
+          amount = scanner.nextDouble();
+          account.deposit(amount);
+          break;
+
+        case 2:
+          System.out.println("How much would you like to withdraw?");
+          amount = scanner.nextDouble();
+          account.withdraw(amount);
+          break;
+
+        case 3:
+          System.out.println("Current balance: " + account.getBalance());
+          break;
+
+        case 4:
+          Authenticator.logout(customer);
+          System.out.println("Thanks for banking at Globe Bank International!");
+          break;
+
+        default:
+          System.out.println("Invalid option. Please try again");
+          break;
+      }
+    }
   }
 }
